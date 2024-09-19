efa49125e033ec03606765f8a5e15f4e7d9e8b81
AlexZ-WP <alexz10@mail.ru>
2024-09-19 19:45:38 +0300

    Add files via upload

--- Modul1-Task2 - variables.py
+++ Modul1-Task2 - variables.py
@@ -1,9 +1,9 @@
 
 #
-Number_of_Home_Tasks = '12'
-number_of_used_hours = '1,5'
+Number_of_Home_Tasks = 12
+number_of_used_hours = 1,5
 course_name = 'Python'
-Time_for_one_task = '0.125'
+Time_for_one_task = 1.5/12
 print( 'Курс:', course_name, ', всего задач:' , Number_of_Home_Tasks , ', затрачено часов:' , number_of_used_hours , ', среднее время выполнения' ,  Time_for_one_task , 'часа')
 
 
